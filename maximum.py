6ec711326cd3142b976d8febdf26cf6b87d753de
AryaTilekar <126436627+AryaTilekar@users.noreply.github.com>
2023-08-03 10:01:33 +0530

    Update maximum.py

--- maximum.py
+++ maximum.py
@@ -1,5 +1,7 @@
 list1={154,90,78,52,60,33,41}
 max_num = max(list1)
+min_num = min(list1)
 print("the list is:")
 print(list1)
 print("maximum number is: " , max_num)
+print("minimum number is: " , min_num)
